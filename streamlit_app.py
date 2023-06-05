724565d8f011cd7ad6c4f4c9e7bf3591755f7087
pmbly <135649439+pmbly@users.noreply.github.com>
2023-06-05 14:23:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,5 @@ streamlit.text('🐔 Hard-Boild Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
